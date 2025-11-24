2f36ed2e1e386c6c3456bf51c8168182ec7cefaf
AdamKenny <122899668+AdamKenny098@users.noreply.github.com>
2025-11-24 16:05:12 +0000

    Filter topic subscribers.
    Added read and write permissions to the lambda which was not in the lab

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -104,6 +104,21 @@ export class EDAAppStack extends cdk.Stack {
         memorySize: 128,
       }
     );
+
+    const addMetadataFn = new lambdanode.NodejsFunction(
+      this,
+      "addMetadataFn",
+    {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            entry: `${__dirname}/../lambdas/addImageMetadata.ts`,
+            timeout: cdk.Duration.seconds(15),
+            memorySize: 128,
+            environment: {
+              TABLE_NAME: imagesTable.tableName,
+            },
+        }
+    );
+
     
 
     // S3 --> SQS
@@ -136,10 +151,56 @@ export class EDAAppStack extends cdk.Stack {
     rejectedImageFn.addEventSource(rejectedImageEventSource);
 
     newImageTopic.addSubscription(
-      new subs.SqsSubscription(imageProcessQueue)
-    );
+      new subs.SqsSubscription(imageProcessQueue, {
+        filterPolicyWithMessageBody: {
+          Records: sns.FilterOrPolicy.policy({
+            s3: sns.FilterOrPolicy.policy({
+              object: sns.FilterOrPolicy.policy({
+                key: sns.FilterOrPolicy.filter(
+                  sns.SubscriptionFilter.stringFilter({
+                    matchPrefixes: ["image"],
+                 })
 
-    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+             ),
+           }),
+         }),
+         }),
+         },
+        rawMessageDelivery: true,
+      })
+ );
+
+
+ newImageTopic.addSubscription(
+  new subs.SqsSubscription(mailerQ, {
+    filterPolicyWithMessageBody: {
+      Records: sns.FilterOrPolicy.policy({
+        s3: sns.FilterOrPolicy.policy({
+          object: sns.FilterOrPolicy.policy({
+            key: sns.FilterOrPolicy.filter(
+              sns.SubscriptionFilter.stringFilter({
+                matchPrefixes: ["image"],
+              })
+            ),
+          }),
+        }),
+       }),
+     },
+    rawMessageDelivery: true,
+  })
+);
+
+
+    newImageTopic.addSubscription(
+      new subs.LambdaSubscription(addMetadataFn, {
+        filterPolicy: {
+          metadata_type: sns.SubscriptionFilter.stringFilter({
+            allowlist: ["Caption", "Date", "Photographer"],
+      }),
+      },
+    })
+ );
+
 
 
 
@@ -160,6 +221,8 @@ export class EDAAppStack extends cdk.Stack {
     );
 
     imagesTable.grantReadWriteData(processImageFn);
+    imagesTable.grantReadWriteData(addMetadataFn);
+
 
 
 
@@ -168,5 +231,10 @@ export class EDAAppStack extends cdk.Stack {
     new cdk.CfnOutput(this, "bucketName", {
       value: imagesBucket.bucketName,
     });
+
+    new cdk.CfnOutput(this, "SNS Topic ARN", {
+      value: newImageTopic.topicArn ,
+     });
+
   }
 }
